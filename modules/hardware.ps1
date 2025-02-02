6cce22f0186bca98ee2c7921af3afeb6eeaa7e9e
phromain <phromain@hotmail.fr>
2025-02-02 20:25:56 +0100

    refactor: Amélio fonctions et information hardware

--- modules/hardware.ps1
+++ modules/hardware.ps1
@@ -1,65 +1,85 @@
 ﻿# Fonction pour récupérer les informations du matériel
-function Show-HardwareInfo {
-    # Obtenir les informations CPU
+
+function Get-ComputerInfo {
+    $computer = Get-WmiObject Win32_ComputerSystem
+    $result = "=== Informations Ordinateur ===`n"
+    $result += "Fabricant : $($computer.Manufacturer)`n"
+    $result += "Modèle : $($computer.Model)`n"
+    return $result
+}
+function Get-CPUInfo {
     $cpu = Get-WmiObject Win32_Processor | Select-Object -First 1
-    $cpuInfo = @{
-        "Modèle" = $cpu.Name
-        "Nombre de cœurs" = $cpu.NumberOfCores
-        "Vitesse" = "$($cpu.MaxClockSpeed) MHz"
-    }
+    $result = "=== CPU ===`n"
+    $result += "Modèle : $($cpu.Name)`n"
+    $result += "Nombre de cœurs : $($cpu.NumberOfCores)`n"
+    $result += "Vitesse : $($cpu.MaxClockSpeed) MHz`n"
+    return $result
+}
 
-    # Obtenir les informations RAM
-    $ram = Get-WmiObject Win32_ComputerSystem
-    $ramInfo = @{
-        "Capacité totale" = "$([math]::round($ram.TotalPhysicalMemory / 1GB, 2)) GB"
-        "Mémoire utilisée" = "$([math]::round((($ram.TotalPhysicalMemory - $ram.FreePhysicalMemory) / 1GB), 2)) GB"
-        "Mémoire disponible" = "$([math]::round($ram.FreePhysicalMemory / 1GB, 2)) GB"
-    }
+function Get-RAMInfo {
+    $ramModules = Get-WmiObject Win32_PhysicalMemory
+    $result = "=== RAM ===`n"
+    $result += "Nombre de modules : $($ramModules.Count)`n"
 
-    # Obtenir les informations GPU
+    foreach ($module in $ramModules) {
+        $type = switch ($module.MemoryType) {
+            20 { "DDR" }
+            21 { "DDR2" }
+            22 { "DDR2 FB-DIMM" }
+            24 { "DDR3" }
+            26 { "DDR4" }
+            default { "Indisponible" }
+        }
+        
+        $result += "Modèle : $($module.Manufacturer)`n"
+        $result += "Capacité : $([math]::round($module.Capacity / 1GB, 2)) GB`n"
+        $result += "Vitesse : $($module.Speed) MHz`n"
+        $result += "Type : $type`n"
+    }
+    return $result
+}
+
+
+function Get-GPUInfo {
     $gpu = Get-WmiObject Win32_VideoController | Select-Object -First 1
-    $gpuInfo = @{
-        "Modèle" = $gpu.Name
-        "Mémoire dédiée" = "$($gpu.AdapterRAM / 1MB) MB"
-        "Version du pilote" = $gpu.DriverVersion
-    }
+    $result = "=== GPU ===`n"
+    $result += "Modèle : $($gpu.Name)`n"
+    $result += "Mémoire dédiée : $($gpu.AdapterRAM / 1MB) MB`n"
+    $result += "Version du pilote : $($gpu.DriverVersion)`n"
+    return $result
+}
 
-    # Obtenir les informations des disques
-    $disks = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 }
-    $diskInfo = @()
+function Get-DiskInfo {
+    $disks = Get-WmiObject Win32_DiskDrive
+    $result = "=== Disques ===`n"
+    $result += "Nombre de disques : $($disks.Count)`n"
+
     foreach ($disk in $disks) {
-        $diskInfo += @{
-            "Nom" = $disk.DeviceID
-            "Capacité totale" = "$([math]::round($disk.Size / 1GB, 2)) GB"
-            "Espace libre" = "$([math]::round($disk.FreeSpace / 1GB, 2)) GB"
-            "Type" = if ($disk.Description -like "*SSD*") { "SSD" } else { "HDD" }
-            "État de santé" = $disk.Status
-        }
-    }
+        $type = if ($disk.MediaType -like "*SSD*") { "SSD" } else { "HDD" }
+        $totalSizeGB = [math]::round($disk.Size / 1GB, 2)
 
-    # Formater les informations en chaîne de caractères
-    $result = "=== Informations matériel ===`n"
-    $result += "=== CPU ===`n"
-    foreach ($key in $cpuInfo.Keys) {
-        $result += "$key : $($cpuInfo[$key])`n"
-    }
-
-    $result += "=== RAM ===`n"
-    foreach ($key in $ramInfo.Keys) {
-        $result += "$key : $($ramInfo[$key])`n"
-    }
-
-    $result += "=== GPU ===`n"
-    foreach ($key in $gpuInfo.Keys) {
-        $result += "$key : $($gpuInfo[$key])`n"
-    }
-
-    $result += "=== Disques ===`n"
-    foreach ($disk in $diskInfo) {
-        foreach ($key in $disk.Keys) {
-            $result += "$key : $($disk[$key])`n"
-        }
+        $result += "Modèle : $($disk.Model)`n"
+        $result += "Type : $type`n"
+        $result += "Capacité totale : $totalSizeGB GB`n"
+        $result += "État de santé : $($disk.Status)`n"
         $result += "`n"
     }
     return $result
 }
+
+function Show-HardwareInfo {
+    $result = ""
+    $result += Get-ComputerInfo
+    $result += "`n"
+    $result += Get-CPUInfo
+    $result += "`n"
+    $result += Get-RAMInfo
+    $result += "`n"
+    $result += Get-GPUInfo
+    $result += "`n"
+    $result += Get-DiskInfo
+    return $result
+}
+
+# Exécuter la fonction et afficher les informations matérielles
+Show-HardwareInfo
