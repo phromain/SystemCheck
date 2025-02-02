63d15751d1531ffc6027ad91805d8831dc1801e1
phromain <phromain@hotmail.fr>
2025-02-02 20:52:28 +0100

    refacto: Amelio fonction Securité

--- modules/security.ps1
+++ modules/security.ps1
@@ -1,38 +1,43 @@
 ﻿# Fonction pour récupérer les informations de sécurité
-function Show-SecurityInfo {
-    # Obtenir l'état du pare-feu
-    $firewall = Get-NetFirewallProfile -PolicyStore ActiveStore
-    $firewallState = $firewall | ForEach-Object {
-        @{
-            Name = $_.Name
-            Enabled = if ($_.Enabled) { "Oui" } else { "Non" }
-        }
+function Get-FirewallInfo {
+    $firewallProfiles = Get-NetFirewallProfile -PolicyStore ActiveStore
+    $result = "=== Pare-feu ===`n"
+    
+    foreach ($profile in $firewallProfiles) {
+        $status = if ($profile.Enabled) { "Activé" } else { "Désactivé" }
+        $result += "$($profile.Name) : $status`n"
     }
+    
+    return $result
+}
 
-    # Obtenir l'antivirus installé et son état
-    $antivirus = Get-WmiObject -Namespace "Root\SecurityCenter2" -Class AntiVirusProduct
-    $antivirusInfo = @()
-    foreach ($av in $antivirus) {
+# Fonction pour récupérer les informations sur l'antivirus
+function Get-AntivirusInfo {
+    $antivirusList = Get-WmiObject -Namespace "Root\SecurityCenter2" -Class AntiVirusProduct
+    $result = "=== Antivirus ===`n"
+    
+    if ($antivirusList.Count -eq 0) {
+        return $result + "Aucun antivirus détecté`n"
+    }
+    
+    foreach ($av in $antivirusList) {
         $productState = [System.Convert]::ToString($av.productState, 16)
-        # Assurez-vous d'interpréter l'état correctement en vérifiant les bits significatifs
-        $state = if ($productState -eq "41000" -or $productState.StartsWith("1")) { "Actif" } elseif ($productState.StartsWith("2")) { "Désactivé" } else { "Inconnu" }
-        $antivirusInfo += @{
-            "Nom" = $av.displayName
-            "État" = $state
-        }
+        $state = if ($productState -eq "41000" -or $productState.StartsWith("1")) { "Actif" } 
+                elseif ($productState.StartsWith("2")) { "Désactivé" } 
+                else { "Inconnu" }
+        $result += "Nom : $($av.displayName)`n"
+        $result += "État : $state`n"
     }
+    
+    return $result
+}
 
-    # Formater les informations en chaîne de caractères
+# Fonction principale pour afficher les informations de sécurité
+function Show-SecurityInfo {
     $result = "=== Informations de sécurité ===`n"
-    foreach ($fw in $firewallState) {
-        $result += "Pare-feu $($fw.Name) activé : $($fw.Enabled)`n"
-    }
-
-    foreach ($av in $antivirusInfo) {
-        $result += "Antivirus : $($av.Nom)`n"
-        $result += "État de l'antivirus : $($av.État)`n"
-    }
-
+    $result += Get-FirewallInfo
+    $result += "`n"
+    $result += Get-AntivirusInfo
     return $result
 }
 
